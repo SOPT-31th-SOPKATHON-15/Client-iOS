b5e9e242e6e23194eaf5396756152b9dee061366
L-j-h-c <ckrgkswnsgh@naver.com>
2022-11-20 02:26:29 +0900

    [Feat] #6 - 캐러셀 구현

--- 31-SOPKATHON-iOS/31-SOPKATHON-iOS/Presentation/HomeScene/HomeVC.swift
+++ 31-SOPKATHON-iOS/31-SOPKATHON-iOS/Presentation/HomeScene/HomeVC.swift
@@ -5,25 +5,92 @@
 //  Created by Junho Lee on 2022/11/20.
 //
 
+import SnapKit
 import UIKit
 
 class HomeVC: UIViewController {
 
+    // MARK: - UI Components
+    
+    private let monthLabel: UILabel = {
+        let label = UILabel()
+        label.text = "22.11"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .white
+        label.backgroundColor = .black
+        label.layer.cornerRadius = 22.adjustedH
+        label.textAlignment = .center
+        label.clipsToBounds = true
+        return label
+    }()
+    
+    lazy var carousel: UICollectionView = {
+        let layout = CarouselLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .clear
+        return cv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.setLayout()
+        self.setDelegate()
+        self.registerCell()
+    }
+}
+
+// MARK: - UI & Layout
+
+extension HomeVC {
+    private func setLayout() {
+        self.view.addSubviews(monthLabel, carousel)
+        
+        monthLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(79.adjustedH)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(74.adjusted)
+            make.height.equalTo(44.adjustedH)
+        }
+        
+        carousel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(monthLabel.snp.bottom).inset(54)
+            make.height.equalTo(300)
+        }
+    }
+}
+
+// MARK: - Methods
+
+extension HomeVC {
+    private func setDelegate() {
+        carousel.delegate = self
+        carousel.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func registerCell() {
+        HomeImageCVC.register(target: carousel)
     }
-    */
+}
 
+// MARK: - UICollectionViewDataSource
+
+extension HomeVC: UICollectionViewDelegate {
+    
+}
+
+
+// MARK: - UICollectionViewDataSource
+
+extension HomeVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeImageCVC.className, for: indexPath) as? HomeImageCVC else { return UICollectionViewCell() }
+        
+        return cell
+    }
 }
