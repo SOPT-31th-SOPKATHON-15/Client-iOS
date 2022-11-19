9155492b46ef9f8dbd86a11874f9659fb2ca0820
dudu <329_143@naver.com>
2022-11-20 06:15:50 +0900

    [Feat] #4 - add 뷰 구현

--- 31-SOPKATHON-iOS/31-SOPKATHON-iOS/Presentation/AddScene/AddVC.swift
+++ 31-SOPKATHON-iOS/31-SOPKATHON-iOS/Presentation/AddScene/AddVC.swift
@@ -6,20 +6,22 @@
 //
 
 import UIKit
+import SnapKit
 
-class AddVC: UIViewController {
+class AddVC: UIViewController, UITextFieldDelegate {
     
     let questionLabel1:UILabel = {
         let label = UILabel()
         label.text = "무엇을 참았나요?"
         label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 16)
+        label.font = UIFont(name: "NotoSansCJKKR-Bold", size: 16)
         return label
     }()
     
     let textField1:UITextField = {
         let textField = UITextField()
         textField.backgroundColor = .white
+        textField.textColor = .black
         return textField
     }()
     
@@ -33,13 +35,14 @@ class AddVC: UIViewController {
         let label = UILabel()
         label.text = "얼마나 참았나요?"
         label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 16)
+        label.font = UIFont(name: "NotoSansCJKKR-Bold", size: 16)
         return label
     }()
     
     let textField2:UITextField = {
         let textField = UITextField()
         textField.backgroundColor = .white
+        textField.textColor = .black
         return textField
     }()
     
@@ -53,7 +56,7 @@ class AddVC: UIViewController {
         let label = UILabel()
         label.text = "원"
         label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 20)
+        label.font = UIFont(name: "NotoSansCJKKR-Bold", size: 20)
         return label
     }()
     
@@ -62,13 +65,14 @@ class AddVC: UIViewController {
         let label = UILabel()
         label.text = "한마디 (0/30)"
         label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 16)
+        label.font = UIFont(name: "NotoSansCJKKR-Bold", size: 16)
         return label
     }()
     
     let textView:UITextView = {
         let textView = UITextView()
-        textView.backgroundColor = .systemGray
+        textView.backgroundColor = .myGrey
+        textView.font = UIFont(name: "NotoSansCJKKR-Bold", size: 16)
         return textView
     }()
     
@@ -76,22 +80,27 @@ class AddVC: UIViewController {
         let button = UIButton()
         button.setTitle("등록하기", for: .normal)
         button.setTitleColor(UIColor.white, for: .normal)
-        button.backgroundColor
+        button.backgroundColor = .myGrey
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(presentToAddPopUpVC), for: .touchUpInside)
         return button
     }()
 
     override func viewDidLoad() {
-        super.viewDidLoad()
         self.view.backgroundColor = .white
         textView.delegate = self
+        self.completionButton(isOn: false)
+        textField1.delegate = self
+        textField2.delegate = self
         setLayout()
+        super.viewDidLoad()
     }
 
 }
 
 extension AddVC {
     private func setLayout(){
-        [questionLabel1, textField1, questionLabel2, textField2, wonLabel, underLineView1, underLineView2, questionLabel3, textView].forEach {
+        [questionLabel1, textField1, questionLabel2, textField2, wonLabel, underLineView1, underLineView2, questionLabel3, textView, registerButton].forEach {
             view.addSubview($0)
         }
         
@@ -104,7 +113,7 @@ extension AddVC {
             $0.top.equalTo(self.questionLabel1.snp.bottom).offset(8)
             $0.leading.equalTo(self.questionLabel1)
             $0.width.equalTo(343)
-            $0.height.equalTo(47)
+            $0.height.equalTo(48)
         }
         
         underLineView1.snp.makeConstraints {
@@ -123,7 +132,7 @@ extension AddVC {
             $0.top.equalTo(self.questionLabel2.snp.bottom).offset(8)
             $0.leading.equalTo(self.questionLabel1)
             $0.width.equalTo(343)
-            $0.height.equalTo(30)
+            $0.height.equalTo(48)
         }
         
         underLineView2.snp.makeConstraints {
@@ -134,7 +143,7 @@ extension AddVC {
         }
         
         wonLabel.snp.makeConstraints {
-            $0.top.equalTo(self.questionLabel2.snp.bottom).offset(8)
+            $0.bottom.equalTo(self.underLineView2.snp.top).offset(-3)
             $0.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading).offset(340)
         }
         
@@ -145,11 +154,17 @@ extension AddVC {
         
         textView.snp.makeConstraints {
             $0.top.equalTo(self.questionLabel3.snp.bottom).offset(8)
-            $0.leading.equalTo(self.questionLabel1)
+            $0.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(16)
             $0.width.equalTo(343)
             $0.height.equalTo(140)
         }
         
+        registerButton.snp.makeConstraints {
+            $0.top.equalTo(self.view.snp.bottom).offset(-80)
+            $0.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
+            $0.width.equalTo(375)
+            $00.height.equalTo(80)
+        }
     }
 }
 
@@ -161,5 +176,28 @@ extension AddVC : UITextViewDelegate {
         let changedText = currentText.replacingCharacters(in: stringRange, with: text)
         questionLabel3.text = "한마디 (\(changedText.count)/30)"
         return changedText.count < 30
+    }
+}
+
+extension AddVC {
+    func completionButton(isOn: Bool){
+        switch isOn{
+        case true:
+            registerButton.isEnabled = true
+            registerButton.backgroundColor = .myBlue
+        case false:
+            registerButton.isEnabled = false
+            registerButton.backgroundColor = .systemGray
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let test = !(textField1.text?.isEmpty ?? true) && !(textField2.text?.isEmpty ?? true)
+        completionButton(isOn: test)
+    }
+    
+    @objc private func presentToAddPopUpVC(){
+        let popupVC = AddPopUpVC()
+        popupVC.modalPresentationStyle = .fullScreen
+        self.present(popupVC, animated: true)
     }
 }
